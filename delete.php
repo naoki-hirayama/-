03d5b826e039525966de2b7cd0652bdf701caa1a
naoki.hirayama <bfmv.na.metalmaster820@gmail.com>
2019-03-07 09:49:02 +0000

    リファクタリング１　ファイル分割

--- delete.php
+++ delete.php
@@ -2,66 +2,9 @@
 
     //MySQLサーバ接続
     require('db_conect.php');
+    
+    include('function_delete.php');
 
-    // SQLインジェクション
-    $get_id = $_GET['id'];//質問する
-    
-    $sql = 'SELECT * FROM post WHERE id = :id';
-            
-    $statement = $database->prepare($sql);
-    
-    $statement->bindParam(':id', $_GET['id']);
-    
-    $statement->execute();
-    
-    $record = $statement->fetch(PDO::FETCH_ASSOC);
-     
-    
-    if (isset($record['id']) === false) {
-        // 404 
-        header( 'HTTP/1.1 404 Not Found' ) ;
-        exit;
-    } elseif (empty($record['password'])) {
-        // 400
-        header('HTTP/1.1 400 Bad Request');
-        exit;
-    } else {
-        
-        $origin_password = $record['password'];
-    }
-    
-    
-    // delete.phpからPOST送信された
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            //パスワードが一致しない、不正時のエラー処理
-            $errors = [];
-            if ($origin_password !== $_POST['password_input']) {
-                $errors[] = "パスワードが違います";
-            }
-            
-            //パスワードが一致した時 
-            if (empty($errors)) {
-                
-                $sql = 'DELETE FROM post WHERE id = :id';
-                
-                $statement = $database->prepare($sql);
-                
-                $statement->bindParam(':id', $_GET['id']);
-                
-                $statement->execute();
-                
-                $statement = null;
-                
-                header('Location: deleted.php');
-                exit;
-            }
-            
-    }
-    
-    $statement = null;
-    
-    $database = null;
-    
 ?>
 <!DOCTYPE html>
 <html lang="ja">
---
+++ function_index.php
@@ -0,0 +1,84 @@
+<?php
+    // index.phpの機能
+    
+    // function h($string) {
+//     return htmlspecialchars($string, ENT_QUOTS, "UTF-8");
+// }
+    //MySQLサーバ接続
+    require('db_conect.php');
+    
+    // セレクトボックスの連想配列
+    $select_options = ['black'=>'黒','red'=>'赤','blue'=>'青','yellow'=>'黄','green'=>'緑'];
+    
+    // POSTでアクセスされたら投稿処理を行う
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $errors = [];
+        
+        // バリデーションを行う
+        if (strlen($_POST['name']) === 0) {
+            $errors[] = "名前は入力必須です。";
+        } else if (strlen($_POST['name']) > 10) {
+            $errors[] = "名前は１０文字以内です。";
+        }
+        
+        if (strlen($_POST['comment']) === 0) {
+            $errors[] = "本文は入力必須です。";
+        } else if (strlen($_POST['comment']) > 100) {
+            $errors[] = "本文は１００文字以内です。";
+        }
+        
+        if (!array_key_exists($_POST['color'], $select_options)) {
+           $errors[] = "文字色が不正です"; 
+        }
+        
+        if (strlen($_POST['password']) !== 0) {
+            if (strlen($_POST['password']) < 4) {
+                $errors[] = " パスワードは4文字以上です。";
+            }
+            
+            if (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['password'])) {
+                $errors[] = " パスワードは半角英数字です。";
+            }
+        }
+        
+        // 成功した場合はDBへ保存してsend.phpにリダイレクトする
+        if (empty($errors)) {
+            $sql = 'INSERT INTO post (name,comment,color,password) VALUES(:name,:comment,:color,:password)';
+            
+            $statement = $database->prepare($sql);
+            
+            //パスワードが入力されない時の処理
+            if (empty($_POST['password'])) {
+                $password = null;
+            } else {
+                $password = $_POST['password'];
+            }
+            
+            $statement->bindParam(':name', $_POST['name']);
+            $statement->bindParam(':comment', $_POST['comment']);
+            $statement->bindParam(':color', $_POST['color']);
+            $statement->bindParam(':password', $password);
+            
+            $statement->execute();
+            
+            $statement = null;
+    
+            header('Location: send.php');
+            exit;
+        }
+    // GETでアクセスされたら一覧表示用にDBから投稿を取得する
+    } else {
+        $sql = 'SELECT * FROM post ORDER BY created_at DESC';
+        
+        $statement = $database->prepare($sql);
+        
+        $statement->execute();
+        
+        $records = $statement->fetchAll();
+    }
+    
+    
+    $statement = null;
+    
+    $database = null;
+    
