6f6d63890dd715f1d46f1dee8cf951df724b7c37
naoki.hirayama <bfmv.na.metalmaster820@gmail.com>
2019-04-02 02:41:40 +0000

    クラス化 postモデル 修正 2

--- class.php
+++ class.php
@@ -2,29 +2,35 @@
 
 class Base 
 {
-    protected $hoge = 'base';
+    protected $add;
     
-    public function hoge($id)
+    public function add($n)
     {
-        return '1'.$id;
+        return 1 + $n;
     }
-}
-
-class Hoge extends Base
-{
-    protected $jjhoge = 'hoge2';
     
-    public function hoge($id)
-    {   
-         $m = parent::hoge($id);
-         
-         echo $id.$m;
+    public function add2($m)
+    {
+        return 1 + $m;
     }
 }
 
-$hoge = new Hoge();
-var_dump($hoge->hoge(2));
+class Add extends Base
+{
+    protected $hoge;
+    
+    public function add($id)
+    {   
+         parent::add($id);
+         echo 1 + $this->add($id);
+    }
+}
 
+$base = new Base();
+var_dump($base->add(2));
+
+$add = new Add();
+var_dump($add->add(5));
 
 // $base = new Base();
 // echo $base->base();
