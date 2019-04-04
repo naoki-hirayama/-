c283ffc646a5443e9c666d72b4632f39ea723377
naoki.hirayama <bfmv.na.metalmaster820@gmail.com>
2019-04-04 01:43:41 +0000

    レス機能　追加　修正4

--- views/deletereply.php
+++ views/deletereply.php
@@ -11,19 +11,21 @@
     <ul>
         <li>
             名前：
-                  <?php if (!empty($reply_post['user_id'])) : ?>
-                      <?php echo h($current_user_name['name']) ?><br />
-                  <?php else : ?>
-                      <?php echo h($reply_post['name']) ?><br />
-                  <?php endif ?>
-            本文：<?php echo h($reply_post['comment']) ?><br />
+            <?php if (!empty($reply_post['user_id'])) : ?>
+                <?php echo h($current_user_name['name']) ?><br />
+            <?php else : ?>
+                <?php echo h($reply_post['name']) ?><br />
+            <?php endif ?>
+            本文：
+            <?php echo h($reply_post['comment']) ?><br />
             画像：
-                  <?php if (!empty($reply_post['picture'])) : ?>
-                      <img src="replyimages/<?php echo h($reply_post['picture']) ?>" width="300" height="200"><br />
-                  <?php else : ?>
-                      なし<br />
-                  <?php endif ?>
-            時間：<?php echo h($reply_post['created_at']) ?><br />
+            <?php if (!empty($reply_post['picture'])) : ?>
+                <img src="replyimages/<?php echo h($reply_post['picture']) ?>" width="300" height="200"><br />
+            <?php else : ?>
+                なし<br />
+            <?php endif ?>
+            時間：
+            <?php echo h($reply_post['created_at']) ?><br />
             ---------------------------------------------<br />
             <form action="deletereply.php?id=<?php echo $reply_post['id'] ?>" method="post">
             <?php if (isset($reply_post['password']) && $reply_post['password'] !== null) : ?>
