15c316d448e74509c4e19809d0e972d9f2106122
naoki.hirayama <bfmv.na.metalmaster820@gmail.com>
2019-04-11 01:20:18 +0000

    git テスト2

--- memo/create_table_bbs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE bbs.post (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(10),
-    comment VARCHAR(100),
-    color VARCHAR(6),
-    delete_password VARCHAR(100),
-    picture  VARCHAR(100),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
