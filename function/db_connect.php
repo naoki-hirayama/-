9e26a1cfcf94ddb873475de32ed8155da1b259c6
naoki-hirayama <bfmv.na.metalmaster820@gmail.com>
2019-04-19 15:53:40 +0900

    git 設定

--- function/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-function db_connect($dbname = 'bbs', $username = 'root', $password = 'Naoki0820-')
-{
-    $database =  new PDO("mysql:host=localhost;dbname={$dbname};charset=utf8mb4;", $username, $password);
-    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $database;
-}
-
